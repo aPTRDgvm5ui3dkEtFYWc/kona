aPTRDgvm5ui3dkEtFYWc/kona

commit 43689a4ee74c5e2f588c0dae60fb675df7111707

fix(bin/node): allow advertised ports to be automatically chosen by the client (#2805)
Closes #1514

---------

Co-authored-by: theo <80177219+theochap@users.noreply.github.com>

bin/node/src/flags/p2p.rs
@@ -53,20 +53,12 @@ pub struct P2PArgs {
     pub advertise_ip: Option<IpAddr>,
     /// TCP port to advertise to external peers from the discovery layer. Same as `p2p.listen.tcp`
     /// if set to zero.
-    #[arg(
-        long = "p2p.advertise.tcp",
-        default_value = "0",
-        env = "KONA_NODE_P2P_ADVERTISE_TCP_PORT"
-    )]
-    pub advertise_tcp_port: u16,
+    #[arg(long = "p2p.advertise.tcp", env = "KONA_NODE_P2P_ADVERTISE_TCP_PORT")]
+    pub advertise_tcp_port: Option<u16>,
     /// UDP port to advertise to external peers from the discovery layer.
     /// Same as `p2p.listen.udp` if set to zero.
-    #[arg(
-        long = "p2p.advertise.udp",
-        default_value = "0",
-        env = "KONA_NODE_P2P_ADVERTISE_UDP_PORT"
-    )]
-    pub advertise_udp_port: u16,
+    #[arg(long = "p2p.advertise.udp", env = "KONA_NODE_P2P_ADVERTISE_UDP_PORT")]
+    pub advertise_udp_port: Option<u16>,
 
     /// IP to bind LibP2P/Discv5 to.
     #[arg(long = "p2p.listen.ip", default_value = "0.0.0.0", env = "KONA_NODE_P2P_LISTEN_IP")]
@@ -260,11 +252,13 @@ impl P2PArgs {
     /// - If the TCP port is already in use.
     /// - If the UDP port is already in use.
     pub fn check_ports(&self) -> Result<()> {
+        let advertise_tcp = self.advertise_tcp_port.unwrap_or(self.listen_tcp_port);
+        let advertise_udp = self.advertise_udp_port.unwrap_or(self.listen_udp_port);
+
         Self::check_ports_inner(
-            // If the advertised ip is not specified, we use the listen ip.
             self.advertise_ip.unwrap_or(self.listen_ip),
-            self.advertise_tcp_port,
-            self.advertise_udp_port,
+            advertise_tcp,
+            advertise_udp,
         )?;
         Self::check_ports_inner(self.listen_ip, self.listen_tcp_port, self.listen_udp_port)?;
 
@@ -362,16 +356,14 @@ impl P2PArgs {
         let static_ip = self.advertise_ip.is_some();
 
         // If the advertise tcp port is null, use the listen tcp port
-        let advertise_tcp_port = if self.advertise_tcp_port != 0 {
-            self.advertise_tcp_port
-        } else {
-            self.listen_tcp_port
+        let advertise_tcp_port = match self.advertise_tcp_port {
+            None => self.listen_tcp_port,
+            Some(port) => port,
         };
 
-        let advertise_udp_port = if self.advertise_udp_port != 0 {
-            self.advertise_udp_port
-        } else {
-            self.listen_udp_port
+        let advertise_udp_port = match self.advertise_udp_port {
+            None => self.listen_udp_port,
+            Some(port) => port,
         };
 
         let keypair = self.keypair().unwrap_or_else(|_| Keypair::generate_secp256k1());

bin/node/src/commands/node.rs
@@ -179,8 +179,18 @@ impl NodeCommand {
                         .map(|ip| ip.to_string())
                         .unwrap_or(String::from("0.0.0.0"))
                 ),
-                (CliMetrics::P2P_ADVERTISE_TCP_PORT, self.p2p_flags.advertise_tcp_port.to_string()),
-                (CliMetrics::P2P_ADVERTISE_UDP_PORT, self.p2p_flags.advertise_udp_port.to_string()),
+                (
+                    CliMetrics::P2P_ADVERTISE_TCP_PORT,
+                    self.p2p_flags
+                        .advertise_tcp_port
+                        .map_or_else(|| "auto".to_string(), |p| p.to_string())
+                ),
+                (
+                    CliMetrics::P2P_ADVERTISE_UDP_PORT,
+                    self.p2p_flags
+                        .advertise_udp_port
+                        .map_or_else(|| "auto".to_string(), |p| p.to_string())
+                ),
                 (CliMetrics::P2P_PEERS_LO, self.p2p_flags.peers_lo.to_string()),
                 (CliMetrics::P2P_PEERS_HI, self.p2p_flags.peers_hi.to_string()),
                 (CliMetrics::P2P_GOSSIP_MESH_D, self.p2p_flags.gossip_mesh_d.to_string()),

commit 1a12adb551016e56123d76369e4cf30a3aff821a

Only check that the listen port & IP is available (#2897)
`check_ports_inner` binds both a UDP and TCP socket to the supplied IP &
PORT. This is invalid for binding to the advertised IP because the
advertised IP is usually an external IP (either or a load balancer or
the user's router when behind NAT at home or the machine's IP when
running in docker) rather than an IP that the kernel is able to bind to.
If the IP is not the same as the local IP of the context in which
kona-node is running, OS Error 99: Cannot assign requested address is
returned.

This fixes a regression introduced in #2805

bin/node/src/flags/p2p.rs
@@ -243,7 +243,7 @@ impl P2PArgs {
         Ok(())
     }
 
-    /// Checks if the ports are available on the system.
+    /// Checks if the listen ports are available on the system.
     ///
     /// If either of the ports are `0`, this check is skipped.
     ///
@@ -252,17 +252,7 @@ impl P2PArgs {
     /// - If the TCP port is already in use.
     /// - If the UDP port is already in use.
     pub fn check_ports(&self) -> Result<()> {
-        let advertise_tcp = self.advertise_tcp_port.unwrap_or(self.listen_tcp_port);
-        let advertise_udp = self.advertise_udp_port.unwrap_or(self.listen_udp_port);
-
-        Self::check_ports_inner(
-            self.advertise_ip.unwrap_or(self.listen_ip),
-            advertise_tcp,
-            advertise_udp,
-        )?;
-        Self::check_ports_inner(self.listen_ip, self.listen_tcp_port, self.listen_udp_port)?;
-
-        Ok(())
+        Self::check_ports_inner(self.listen_ip, self.listen_tcp_port, self.listen_udp_port)
     }
 
     /// Returns the private key as specified in the raw cli flag or via file path.